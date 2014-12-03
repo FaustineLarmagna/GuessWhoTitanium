67eb8f41d3cc8ec79b452d8ab71c7835eae313fb
FaustineLarmagna <f.larmagna@gmail.com>
2014-12-03 09:10:16 +0100

    Removed Resources folder from index

--- Resources/android/alloy/styles/characterPictureTemplate.js
+++
@@ -1,1 +0,0 @@
-module.exports = [{"isId":true,"priority":100000.0006,"key":"characterPicture","style":{width:Alloy.Globals.characterWidth,height:Alloy.Globals.characterWidth,}}];
--- Resources/mobileweb/app.js
+++
@@ -1,9 +0,0 @@
-var Alloy = require("alloy"), _ = Alloy._, Backbone = Alloy.Backbone;
-
-Alloy.Globals.characterWidth = Ti.Platform.displayCaps.platformWidth / 4;
-
-var initialization = require("initialization");
-
-initialization.initializeGame();
-
-Alloy.createController("index");
